54293d3b3073566629ed7c32edd7211c3a4df13a
taako-502 <i.am.armorknight@gmail.com>
2023-05-29 23:50:07 +0900

    test: rspec:install

---
+++ back/spec/README.md
@@ -0,0 +1,11 @@
+# Test
+## テスト方法
+### 全体
+```bash
+$ docker-compose run back bundle exec rspec
+```
+
+### 個別のファイルに対して
+```bash
+$ docker-compose run back bundle exec rspec spec/~~~
+```
---
+++ back/spec/controllers/api/users_controller_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe Api::UsersController, type: :controller do
+  let(:user) { create(:user) }
+
+  describe "GET #index" do
+    before do
+      get :index
+    end
+
+    it "returns a successful response" do
+      expect(response).to be_successful
+    end
+
+    it "returns all users" do
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq(User.count)
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      get :show, params: { id: user.id }
+    end
+
+    it "returns a successful response" do
+      expect(response).to be_successful
+    end
+
+    it "returns the user" do
+      json_response = JSON.parse(response.body)
+      expect(json_response["id"]).to eq(user.id)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      let(:valid_attributes) { { name: Faker::Name.name } }
+
+      it "creates a new user" do
+        expect {
+          post :create, params: { user: valid_attributes }
+        }.to change(User, :count).by(1)
+      end
+    end
+
+    context "with invalid attributes" do
+      let(:invalid_attributes) { { name: nil } }
+
+      it "does not create a new user" do
+        expect {
+          post :create, params: { user: invalid_attributes }
+        }.to change(User, :count).by(0)
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    let(:new_name) { Faker::Name.name }
+
+    before do
+      patch :update, params: { id: user.id, user: { name: new_name } }
+    end
+
+    it "updates the user" do
+      user.reload
+      expect(user.name).to eq(new_name)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "deletes the user" do
+      user_to_delete = create(:user)
+      expect {
+        delete :destroy, params: { id: user_to_delete.id }
+      }.to change(User, :count).by(-1)
+    end
+  end
+end
