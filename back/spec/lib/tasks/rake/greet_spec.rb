18129f7f85138842d8f6d869153dcdac1847c154
taako-502 <i.am.armorknight@gmail.com>
2023-06-05 01:58:57 +0900

    have_receivedを使ったテストを追加

--- back/spec/lib/tasks/rake/greet_spec.rb
+++ back/spec/lib/tasks/rake/greet_spec.rb
@@ -19,9 +19,19 @@ RSpec.describe 'greet' do
 
 
   describe 'greet:mock_test' do
-    it 'greet.mock_test' do
+    it 'greet.mock_test 1' do
       expect_any_instance_of(Greet).to receive(:test).and_return('mocked_test_result')
       expect { Rake::Task['greet:mock_test'].invoke }.to output("mocked_test_result\n").to_stdout
+    end
+
+    it 'greet.mock_test 2' do
+      mock_greet = instance_double(Greet)
+      allow(Greet).to receive(:new).and_return(mock_greet)
+      allow(mock_greet).to receive(:test).and_return('mocked_test_result')
+      
+      Rake::Task['greet:mock_test'].reenable
+      expect { Rake::Task['greet:mock_test'].invoke }.to output("mocked_test_result\n").to_stdout
+      expect(mock_greet).to have_received(:test)
     end
   end
 
