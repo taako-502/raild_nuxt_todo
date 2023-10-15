b75493cdbb7c27109f6c2d0a3d6a366b0bc84c37
taako-502 <i.am.armorknight@gmail.com>
2023-10-15 21:45:11 +0300

    fix: ユーザを削除する機能を作成

--- front/pages/users/[id].vue
+++ front/pages/users/[id].vue
@@ -1,13 +1,17 @@
 <template>
   <h1>Hello, {{ name }}</h1>
+  <div>
+    <Button :onClick="remove">Remove</Button>
+  </div>
   <NuxtLink to="/users/new">Back</NuxtLink>
   <NuxtLink to="/">Back to Top</NuxtLink>
 </template>
 
 <script setup type="ts">
 const name = ref("")
+const { $repository } = useNuxtApp();
+
 const nameFromDB = async () => {
-  const { $repository } = useNuxtApp();
   const axios = $repository.getAxios();
   const route = useRoute();
   const id = route.path.split("/")[2];
@@ -16,4 +20,18 @@ const nameFromDB = async () => {
   name.value = response.data.name
 }
 nameFromDB()
+
+const remove = async () => {
+  if (!window.confirm('削除しますか？')) return
+
+  const axios = $repository.getAxios();
+  const route = useRoute();
+  const id = route.path.split("/")[2];
+  try {
+    await axios.delete(`/api/users/${id}`)
+    navigateTo("/")
+  } catch (e) {
+    alert(e)
+  }
+}
 </script>
