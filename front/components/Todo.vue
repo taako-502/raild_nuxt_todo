98800aba3976686ba0cb475aac4d702d805adc44
taako-502 <i.am.armorknight@gmail.com>
2023-10-17 15:45:22 +0300

    refactor: REST APIを実行するのは親

--- front/components/Todo.vue
+++ front/components/Todo.vue
@@ -1,37 +1,25 @@
 <template>
   <form @submit.prevent="post">
     <label for="content">Todo: </label>
-    <input id="content" v-model="content" type="text" name="content" />
+    <input
+      id="content"
+      type="text"
+      name="content"
+      :value="content"
+      @input="emit('update:content', ($event.target as HTMLInputElement).value)"
+    />
     <button type="submit">submit</button>
   </form>
 </template>
 
 <script setup lang="ts">
-import { AxiosError } from 'axios'
-
 const props = defineProps<{
   user_id: number
+  content: string
+  post: () => Promise<void>
 }>()
 
-const content = ref('')
-const contentErrors = ref([])
-
-const post = async () => {
-  const { $repository } = useNuxtApp()
-  const axios = $repository.getAxios()
-  contentErrors.value = []
-  try {
-    // 親コンポネントでAPIを叩く
-    const response = await axios.post(`/api/users/${props.user_id}/tasks`, {
-      content: content.value,
-    })
-    content.value = ''
-  } catch (e: unknown) {
-    console.error(e)
-    if (!(e instanceof AxiosError)) {
-      return
-    }
-    // contentErrors.value = e.response?.data?.content
-  }
-}
+const emit = defineEmits<{
+  (event_: 'update:content', text: string): void
+}>()
 </script>
